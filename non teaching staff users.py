c64d151cdf1f51926acfd6a8469b952ae0ff0ccf
M-himabindu <112838834+M-himabindu@users.noreply.github.com>
2022-09-22 19:55:13 +0530

    Update non teaching staff users.py

--- non teaching staff users.py
+++ non teaching staff users.py
@@ -1,4 +1,4 @@
-a=float(input("Enter Total Number of Users : "))
-b=float(input("Enter Number of Staff Users : "))
+a=int(input("Enter Total Number of Users : "))
+b=int(input("Enter Number of Staff Users : "))
 c=a-b-(b//3)
 print("Student Users Are : " , c)
